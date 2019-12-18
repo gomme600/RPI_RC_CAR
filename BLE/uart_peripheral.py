b2f29edd75892807c6622c8a9137a22c3fec820f
Sebastian L <gomme600@hotmail.com>
2019-12-18 09:25:38 +0100

    bluetooth controls added

--- BLE/uart_peripheral.py
+++ BLE/uart_peripheral.py
@@ -1,10 +1,29 @@
+import RPi.GPIO as GPIO # always needed with RPi.GPIO  
+import time
 import sys
 import dbus, dbus.mainloop.glib
+import subprocess
 from gi.repository import GObject
 from example_advertisement import Advertisement
 from example_advertisement import register_ad_cb, register_ad_error_cb
 from example_gatt_server import Service, Characteristic
 from example_gatt_server import register_app_cb, register_app_error_cb
+
+'''car control setup'''
+GPIO.setmode(GPIO.BOARD)  # choose BCM or BOARD numbering schemes. We use BOARD  
+  
+GPIO.setup(38, GPIO.OUT) # set GPIO 38 as an output.  
+GPIO.setup(36, GPIO.OUT) # set GPIO 36 as an output.
+GPIO.setup(31, GPIO.OUT) # set GPIO 31 as an output.
+GPIO.setup(29, GPIO.OUT) # set GPIO 29 as an output.
+
+FR = GPIO.PWM(38, 1000)    # create an object for PWM on port 38 at 1000 Hertz
+BR = GPIO.PWM(36, 1000)    # create an object for PWM on port 36 at 1000 Hertz
+FL = GPIO.PWM(29, 1000)    # create an object for PWM on port 29 at 1000 Hertz
+BL = GPIO.PWM(31, 1000)    # create an object for PWM on port 31 at 1000 Hertz  
+                        # you can have more than one of these, but they need  
+                        # different names for each port   
+                        # e.g. p1, p2, motor, servo1 etc.
  
 BLUEZ_SERVICE_NAME =           'org.bluez'
 DBUS_OM_IFACE =                'org.freedesktop.DBus.ObjectManager'
@@ -16,7 +35,8 @@ UART_RX_CHARACTERISTIC_UUID =  '00002101-0000-1000-8000-00805f9b34fb'
 UART_TX_CHARACTERISTIC_UUID =  '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
 LOCAL_NAME =                   'RCCAR-PI'
 mainloop = None
- 
+
+''' 
 class TxCharacteristic(Characteristic):
     def __init__(self, bus, index, service):
         Characteristic.__init__(self, bus, index, UART_TX_CHARACTERISTIC_UUID,
@@ -49,19 +69,126 @@ class TxCharacteristic(Characteristic):
         if not self.notifying:
             return
         self.notifying = False
- 
+''' 
 class RxCharacteristic(Characteristic):
     def __init__(self, bus, index, service):
         Characteristic.__init__(self, bus, index, UART_RX_CHARACTERISTIC_UUID,
                                 ['write'], service)
- 
+    def bytes(num):
+        return hex(num >> 8), hex(num & 0xFF)    
+
     def WriteValue(self, value, options):
-        print('remote: {}'.format(bytearray(value).decode()))
+       
+        try:
+            auto_drive_on = False
+            print("------")
+            print("Data Received !")
+            print("------")
+            #print('remote: {}'.format(bytearray(value).decode()))
+            #print("is str")
+
+            #print(value)
+            #print("is raw")        
+            
+            lowb = int(value[0])
+            highb = int(value[1])
+            if((highb < 100) and (highb > 0)):
+                if (auto_drive_on == True):
+                    auto_drive.kill()
+                    auto_drive_on = False
+
+                print("Fowards !")
+                FR.stop()
+                FL.stop()
+                BR.stop()
+                BL.stop()
+                power = highb
+                print("Power : ")
+                print(power)
+                FR.start(power)
+                FL.start(power)
+
+            if( (highb < 256) and (highb > 156) ):
+                if (auto_drive_on == True):
+                    auto_drive.kill()
+                    auto_drive_on = False
+
+                print("Backwards !")
+                FR.stop()
+                FL.stop()
+                BR.stop()
+                BL.stop()
+                power = (((-1)*highb)+256)
+                print("Power : ")
+                print(power)
+                BR.start(power)
+                BL.start(power)
+
+            if( (lowb < 256) and (lowb > 156) ):
+                if (auto_drive_on == True):
+                    auto_drive.kill()
+                    auto_drive_on = False
+
+                print("Left !")
+                FR.stop()
+                FL.stop()
+                BR.stop()
+                BL.stop()
+                power = (((-1)*lowb)+256)
+                print("Power : ")
+                print(power)
+                FR.start(power)
+                BL.start(power)
+
+            if( (lowb < 100) and (lowb > 0) ):
+                if (auto_drive_on == True):
+                    auto_drive.kill()
+                    auto_drive_on = False
+
+                print("Right !")
+                FR.stop()
+                FL.stop()
+                BR.stop()
+                BL.stop()
+                power = lowb
+                print("Power : ")
+                print(power)
+                BR.start(power)
+                FL.start(power)
+
+            if( (highb == 0) and (lowb == 0) ):
+                if (auto_drive_on == True):
+                    auto_drive.kill()
+                    auto_drive_on = False      
+
+                print("Stop !")
+                FR.stop()
+                FL.stop()
+                BR.stop()
+                BL.stop()
+
+            if( (highb == 100) ):
+                if (auto_drive_on == True):
+                   auto_drive.kill()
+                   auto_drive_on = False  
+
+                print("Autonomous !")
+                FR.stop()
+                FL.stop()
+                BR.stop()
+                BL.stop()
+                auto_drive = subprocess.Popen("exec " + "python auto_control_test_2.py", stdout=subprocess.PIPE, shell=True)
+                auto_drive_on = True
+
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print (message)
  
 class UartService(Service):
     def __init__(self, bus, index):
         Service.__init__(self, bus, index, UART_SERVICE_UUID, True)
-        self.add_characteristic(TxCharacteristic(bus, 0, self))
+        #self.add_characteristic(TxCharacteristic(bus, 0, self))
         self.add_characteristic(RxCharacteristic(bus, 1, self))
  
 class Application(dbus.service.Object):
@@ -97,7 +224,7 @@ class UartAdvertisement(Advertisement):
         self.add_service_uuid(UART_SERVICE_UUID)
         self.add_local_name(LOCAL_NAME)
         self.include_tx_power = True
- 
+''' 
 def find_adapter(bus):
     remote_om = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, '/'),
                                DBUS_OM_IFACE)
@@ -108,7 +235,19 @@ def find_adapter(bus):
                 continue
         return o
     return None
- 
+'''
+
+def find_adapter(bus):
+
+    remote_om = dbus.Interface(bus.get_object(BLUEZ_SERVICE_NAME, '/'),DBUS_OM_IFACE)
+    objects = remote_om.GetManagedObjects()
+
+    for o, props in objects.items():
+        if LE_ADVERTISING_MANAGER_IFACE in props:
+            return o
+
+    return None
+
 def main():
     global mainloop
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
